1c1a94010114d995ceec0c7e9a55cb5e7d2cc310
cuiwang <417787204@qq.com>
2018-12-12 11:00:04 +0800

    add

--- pages/sample/index/index.js
+++ pages/sample/index/index.js
@@ -1,11 +1,17 @@
 // pages/sample/index/index.js
+import { errDialog } from '../../../utils/util';
+import { sampleService } from '../shared/service.js';
+import { constant } from '../../../utils/constant';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    storeId: wx.getStorageSync(constant.STORE_INFO) ||'1531800050458194516965',
+    worksList: [],
+    imageWidth: 168,
   },
 
   /**
@@ -26,7 +32,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    getWorkList.call(this);
   },
 
   /**
@@ -62,5 +68,100 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  goWorkDetail(e) {
+    let type = e.currentTarget.dataset.type
+    console.log(e);
+    if (type === 'VIDEO') {
+      getData.call(this, e.currentTarget.dataset.id);
+      
+    } else {
+      let url = ''
+      e.currentTarget.dataset.list.forEach(function(i){
+        url += (i.sourceId+',')
+      })
+      wx.navigateTo({
+        url: '/pages/sample/workDetail/workDetail?imgUrl=' + url,
+      })
+    }
   }
 })
+
+function getWorkList() {
+  let data = {
+    id: this.data.storeId,
+    type: 'STORE'
+  };
+  let self = this;
+  sampleService.getStoreWorkList(data).subscribe({
+    next: res => {
+      this.setData({
+        worksList: workDataFun(res, self.data.imageWidth)
+      })
+    },
+    error: err => errDialog(err),
+    complete: () => wx.hideToast()
+  })
+}
+function getData(productionId) {
+  let data = {
+    productionId: productionId
+  }
+  let self = this;
+  sampleService.getStaffProductionDetail(data).subscribe({
+    next: res => {
+      let imageList = []
+      res.production.merchantMediaDTOS.forEach(function (item, i) {
+        imageList.push({
+          sourceId: item.sourceId
+        })
+      })
+      let videoId = imageList[0].sourceId.split(',')[1];
+      showVideo.call(this, videoId);
+
+    },
+    error: err => errDialog(err),
+    complete: () => wx.hideToast()
+  })
+}
+
+function showVideo(videoId) {
+  let data = {
+    videoId: videoId
+  }
+  sampleService.getVideoUrlById(data).subscribe({
+    next: res => {
+      this.setData({
+        showVideo: true,
+        src: res
+      })
+      wx.navigateTo({
+        url: '/pages/sample/workDetail/workDetail?src=' + res,
+      })
+    },
+    error: err => errDialog(err),
+    complete: () => wx.hideToast()
+  })
+}
+function workDataFun(data, imgW) {
+  let resData = [];
+  data.forEach(function (item) {
+    resData.push({
+      productionId: item.productionId,
+      name: item.title,
+      sourceType: item.sourceType,
+      picId: item.sourceType === 'VIDEO' ? item.merchantMediaDTOS[0].sourceId.split(',')[0] : item.merchantMediaDTOS[0].sourceId,
+      merchantMediaDTOS: item.merchantMediaDTOS
+    })
+  })
+
+  resData.forEach(function (item) {
+    let index = item.picId.lastIndexOf('_');
+    // let picId = item.picId.slice(0, index);
+    let scale = item.picId.slice(index + 1, item.picId.length);
+    item.height = Math.floor(imgW / scale);
+    item.url = constant.OSS_IMAGE_URL + `${item.picId}/resize_${imgW}_${item.height}/mode_fill`
+  })
+
+  return resData;
+}
---
+++ pages/sample/workDetail/workDetail.js
@@ -0,0 +1,116 @@
+import { sampleService } from '../shared/service.js';
+import { constant } from '../../../utils/constant';
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    storeId: wx.getStorageSync(constant.STORE_INFO),
+    getUserInfo: true,
+    bigImage: '/asset/images/pintuan_head1.jpg',
+    worksList: [],
+    imageWidth: 375,
+    showVideo: false,
+    src: '',
+    productionId: '',
+    title: ''  ,
+    imgsrc:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '视频详情',
+    })
+    this.setData({
+      src: options.src || '',
+      storeId: wx.getStorageSync(constant.STORE_INFO) || '1531800050458194516965'
+    })
+    if (options.imgUrl){
+      let list = options.imgUrl.split(',');
+      this.setData({
+        imgsrc: workDataFun.call(this, list)
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  
+
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function (res) {
+    return {
+      title: wx.getStorageSync('storeName'),
+      path: '/pages/shop/video/detail/detail?type=share&productionId=' + this.data.productionId + '&storeId=' + this.data.storeId,
+      success: function (res) {
+        console.log(res);
+      },
+      fail: function (res) {
+        console.log(res);
+      }
+    }
+  }
+})
+function workDataFun(data) {
+  let resData = [];
+  data.forEach(function (item) {
+    resData.push({
+      picId:  item
+    })
+  })
+  resData.forEach(function (item) {
+    let index = item.picId.lastIndexOf('_');
+    let scale = item.picId.slice(index + 1, item.picId.length);
+    item.height = Math.floor(375 / scale);
+    item.url = constant.OSS_IMAGE_URL + `${item.picId}/resize_${375}_${item.height}/mode_fill`
+  })
+  return resData;
+}
